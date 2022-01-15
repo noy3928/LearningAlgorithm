48667c0316abf662dcc97f5f7af4671bef6f835c
yechan <noy3928@naver.com>
2022-01-16 00:02:24 +0900

    prob-1 :세수중 최솟값

---
+++ 인프런/기본문제/세수중최솟값.js
@@ -0,0 +1,17 @@
+function solution(a, b, c){
+    let answer;
+
+    if(a<b){
+        answer = a
+    }else{
+        answer = b
+    }
+
+    if(answer > c){
+        answer = c
+    }
+
+    return answer;
+}
+
+console.log(solution(12, 5, 13));
