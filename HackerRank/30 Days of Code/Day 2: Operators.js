4cc28683b37fdb15f13c4b0b4d107ec8655064c5
yechan <noy3928@naver.com>
2022-01-25 07:10:32 +0900

    #19 : Operators

---
+++ HackerRank/30 Days of Code/Day 2: Operators.js
@@ -0,0 +1,33 @@
+/*Objective
+In this challenge, you will work with arithmetic operators. Check out the Tutorial tab for learning materials and an instructional video.
+
+Task
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.
+
+Example
+
+
+
+A tip of 15% * 100 = 15, and the taxes are 8% * 100 = 8. Print the value  and return from the function.
+
+Function Description
+Complete the solve function in the editor below.
+
+solve has the following parameters:
+
+int meal_cost: the cost of food before tip and tax
+int tip_percent: the tip percentage
+int tax_percent: the tax percentage
+Returns The function returns nothing. Print the calculated value, rounded to the nearest integer.
+
+Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result.
+*/
+
+
+function solve(meal_cost, tip_percent, tax_percent) {
+    // Write your code here
+    const tip = meal_cost * (tip_percent/100)
+    const tax = meal_cost * (tax_percent/100)
+    const sum = Math.round(meal_cost + tip + tax)
+    console.log(sum)
+}
