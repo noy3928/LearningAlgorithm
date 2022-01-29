79b4840e1a10b1674388f8cce3017cb6f0dbf550
yechan <noy3928@naver.com>
2022-01-29 11:58:20 +0900

    #32 : Loops - edit

--- HackerRank/30 Days of Code/Day 5: Loops.js
+++ HackerRank/30 Days of Code/Day 5: Loops.js
@@ -10,6 +10,6 @@ function main() {
     const n = parseInt(readLine().trim(), 10);
     
     for(let i=1; i<11; i++){
-        console.log(`2 x ${i} = ${2*i}`)
+        console.log(`${n} x ${i} = ${n*i}`)
     }
 }
