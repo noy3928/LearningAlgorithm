08d7cb03fbaa3c00c714095532325bd7d71616ea
yechan <noy3928@naver.com>
2022-01-30 15:28:46 +0900

    #38 : Array

---
+++ Practice/HackerRank/30 Days of Code/Day 7: Array.js
@@ -0,0 +1,24 @@
+/*Objective
+Today, we will learn about the Array data structure. Check out the Tutorial tab for learning materials and an instructional video.
+
+Task
+Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
+
+Example
+
+
+Print 4 3 2 1. Each integer is separated by one space.
+
+Input Format
+
+The first line contains an integer,  (the size of our array).
+The second line contains  space-separated integers that describe array 's elements.
+*/
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    const reversedArr = arr.reverse().join(' ')
+    console.log(reversedArr)
+}
