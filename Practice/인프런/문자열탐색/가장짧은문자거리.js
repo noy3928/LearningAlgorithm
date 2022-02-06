bf82343220a2db8d5643520375bdfb9b5ebb2d2b
yechan <noy3928@naver.com>
2022-02-06 23:12:29 +0900

    #48 : 가장짧은문자거리

---
+++ Practice/인프런/문자열탐색/가장짧은문자거리.js
@@ -0,0 +1,30 @@
+/*
+한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출 력하는 프로그램을 작성하세요.
+*/
+
+function solution(s, t){
+    let answer=[];
+    let p=1000;
+    for(let x of s){
+        if(x===t){
+            p=0;
+            answer.push(p);
+        }
+        else{
+            p++;
+            answer.push(p);
+        }
+    }
+    p=1000;
+    for(let i=s.length-1; i>=0; i--){
+        if(s[i]===t) p=0;
+        else{
+            p++;
+            answer[i]=Math.min(answer[i], p);
+        }
+    }
+    return answer;
+}
+
+let str="teachermode";
+console.log(solution(str, 'e'));
