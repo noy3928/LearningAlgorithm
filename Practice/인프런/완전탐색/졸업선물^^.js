301ec1d81e1ac3def6e59e0b77f2f067a1c5b081
yechan <noy3928@naver.com>
2022-03-04 08:30:05 +0900

    #64 : 졸업선물 복습

--- Practice/인프런/완전탐색/졸업선물^^.js
+++ Practice/인프런/완전탐색/졸업선물^^.js
@@ -50,3 +50,30 @@ function solution(m, product){
 
     return answer;
 }
+
+
+//-------------------------
+
+
+function solution(m, product){
+    let answer=0;    
+    let n = product.length
+    product.sort((a,b) => (a[0] + a[1]) - (b[0] + b[1]))
+
+    for(let i = 0; i < n; i ++){
+        let restMoney = m - ((product[i][0]/2) + product[i][1])
+        let cnt = 1 
+        for(let j = 0; j < n; j++){
+            const eachStudentPrice = product[j][0] + product[j][1]
+            if(j !== i && eachStudentPrice > restMoney) break 
+            if(j !== i && eachStudentPrice <= restMoney) {
+                restMoney -= eachStudentPrice
+                cnt ++ 
+            } 
+        }
+        answer = Math.max(answer, cnt)
+    }
+    return answer;
+}
+
+let arr=[[6, 6], [2, 2], [4, 3], [4, 5], [10, 3]];
