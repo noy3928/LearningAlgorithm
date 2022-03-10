7eaef0048367f4d18d1e9931c2c8dba7a5752de0
yechan <noy3928@naver.com>
2022-03-10 10:57:31 +0900

    더 개발자 : 배열내 랜덤한 수를 정렬하는 기발한 방법

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 let fs = require("fs");
-let input = fs.readFileSync("example.txt").toString().trim().split("\n");
-
-const [N, M] = input
-  .shift()
+let input = fs
+  .readFileSync("example.txt")
+  .toString()
+  .trim()
   .split(" ")
   .map((v) => Number(v));
 
-const arr = input
-  .shift()
-  .split(" ")
-  .map((v) => Number(v));
+const solution = (numbers) => {
+  let booleans = Array.from({ length: 100 }, () => false);
 
-const solution = (m, arr) => {
-  let sum = (answer = 0);
-  for (let lt = 0; lt < arr.length; lt++) {
-    let rt = lt + 1;
-    sum = arr[lt];
-    while (sum <= m) {
-      if (sum === m) {
-        answer++;
-      }
-      sum += arr[rt++];
-    }
-    sum = 0;
+  for (num of numbers) {
+    booleans[num] = true;
   }
-  console.log(answer);
+
+  let index = 0;
+  for (let i = 0; i < booleans.length; i++) {
+    if (booleans[i]) {
+      numbers[index++] = i;
+    }
+  }
+  console.log(numbers);
 };
 
-solution(M, arr);
+solution(input);
