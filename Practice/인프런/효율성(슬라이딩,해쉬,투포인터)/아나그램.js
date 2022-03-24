6f0713b786b8c9f31764a1ee493fa546b5ab7cb9
yechan <noy3928@naver.com>
2022-03-24 11:11:01 +0900

    #76 : 아나그램^2

--- Practice/인프런/효율성(슬라이딩,해쉬,투포인터)/아나그램.js
+++ Practice/인프런/효율성(슬라이딩,해쉬,투포인터)/아나그램.js
@@ -15,3 +15,54 @@ function solution(str1, str2) {
 let a = "AbaAeCe";
 let b = "baeeACA";
 console.log(solution(a, b));
+
+//----------------------------------------------------------------
+
+/*
+1.각 문자열의 갯수를 구한 값을 가지고 있어야한다.(객체)
+2.두 개의 객체가 주어지면, 이 두개의 객체가 같은지 비교해야 한다. 
+
++대소문자는 어떻게 해야하는 건가? 
+*/
+
+function solution(str1, str2) {
+  let answer = "YES";
+  let arr1 = new Map();
+  let arr2 = new Map();
+
+  for (const x of str1) {
+    if (arr1.has(x)) {
+      let item = arr1.get(x) + 1;
+      arr1.set(x, item);
+    } else {
+      arr1.set(x, 1);
+    }
+  }
+
+  for (const x of str2) {
+    if (arr2.has(x)) {
+      let item = arr2.get(x) + 1;
+      arr2.set(x, item);
+    } else {
+      arr2.set(x, 1);
+    }
+  }
+
+  for (const [key, value] of arr1) {
+    if (arr2.has(key)) {
+      const arr2Value = arr2.get(key);
+      console.log(arr2Value, value);
+      if (arr2Value !== value) {
+        answer = "NO";
+      }
+    } else {
+      answer = "NO";
+    }
+  }
+
+  return answer;
+}
+
+let c = "abaCC";
+let d = "Caaab";
+console.log(solution(c, d));
