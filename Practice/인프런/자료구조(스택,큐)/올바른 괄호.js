9b81b05a1b0d544fef9a1f4cf262dee8a791ea3f
yechan <noy3928@naver.com>
2022-04-11 08:58:53 +0900

    #86 : 올바른 괄호(복습)

--- Practice/인프런/자료구조(스택,큐)/올바른 괄호.js
+++ Practice/인프런/자료구조(스택,큐)/올바른 괄호.js
@@ -1,3 +1,8 @@
+/*
+괄호가 입력되면 올바른 괄호이면 “YES", 올바르지 않으면 ”NO"를 출력합니다.
+(())() 이것은 괄호의 쌍이 올바르게 위치하는 거지만, (()()))은 올바른 괄호가 아니다.
+*/
+
 function solution(s) {
   let answer = "YES";
   stack = [];
@@ -14,3 +19,29 @@ function solution(s) {
 
 let a = "(()(()))(()";
 console.log(solution(a));
+
+let fs = require("fs");
+let input = fs.readFileSync("example.txt").toString();
+
+/**
+ * 1.괄호를 배열에 넣는다.
+ * 2.(가 나오면
+ */
+
+const solution = (text) => {
+  const stack = [];
+  for (const x of text) {
+    if (x === "(") {
+      stack.push(x);
+    } else {
+      if (stack.length === 0) {
+        return console.log("NO");
+      }
+      stack.pop();
+    }
+  }
+  if (stack.length > 0) return console.log("NO");
+  else return console.log("YES");
+};
+
+solution(input);
