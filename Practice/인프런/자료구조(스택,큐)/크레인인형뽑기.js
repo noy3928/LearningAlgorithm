f699305d83aabe3d275ff97f4320dcf9bbe69f45
yechan <noy3928@naver.com>
2022-04-12 10:42:42 +0900

    #88 : 크레인 인형 뽑기

--- Practice/인프런/자료구조(스택,큐)/크레인인형뽑기.js
+++ Practice/인프런/자료구조(스택,큐)/크레인인형뽑기.js
@@ -28,3 +28,38 @@ let a = [
 
 let b = [1, 5, 3, 5, 1, 2, 1, 4];
 console.log(solution(a, b));
+
+const board = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+];
+const moves = [1, 5, 3, 5, 1, 2, 1, 4];
+
+const solution = (board, moves) => {
+  const bucket = [];
+  let answer = 0;
+
+  for (const x of moves) {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][x - 1] !== 0) {
+        if (
+          bucket.length !== 0 &&
+          bucket[bucket.length - 1] == board[i][x - 1]
+        ) {
+          bucket.pop();
+          answer += 2;
+        } else {
+          bucket.push(board[i][x - 1]);
+        }
+        board[i][x - 1] = 0;
+        break;
+      }
+    }
+  }
+  console.log(answer);
+};
+
+solution(board, moves);
--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 let fs = require("fs");
-let input = fs.readFileSync("example.txt").toString();
+let input = fs.readFileSync("example.txt").toString().trim().split("\n");
+
+const [board, moves] = input;
+console.log(typeof board);
 
 /**
- * 1.괄호 안의 문자열을 제거해야 한다.
- * 2.문자가 괄호안에 있는지 없는지를 판단해야 함
+ * 1.moves에 있는 숫자는 탐색할 열을 의미한다.
+ * 2.moves에 있는 숫자에 해당하는 열로 이동해서, 그 열을 순차적으로 순회한다.
+ * 3.순회하면서 0이 아닌 숫자를 만나면, 그 숫자를 빼서 bucket에 넣어준다. 그리고 원래 있던 자리에 0을 넣어준다.
+ * 4.bucket에 넣어줄 때, 마지막 숫자와 지금 들어올 숫자가 같다면, answer 를 +1 한다. 그리고 지금 들어가는 숫자와 마지막에 있던 숫자를 제거한다.
+ * 5.이 루틴을 반복한다.
  */
 
-const solution = (str) => {
-  let stack = [];
-  let answer = [];
-  for (const x of str) {
-    if (x === "(") stack.push(x);
-    else if (x === ")") stack.pop();
-    else {
-      if (stack.length === 0) {
-        answer.push(x);
-      }
-    }
-  }
-  console.log(answer.join(""));
-};
-
-solution(input);
+const solution = () => {};
