28e7aa724633b904f71602a3490ae5001935b8fc
yechan <noy3928@naver.com>
2022-04-16 23:06:32 +0900

    #95 : LRU

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ let input = fs
   .toString()
   .split(" ")
   .map((v) => Number(v));
+console.log(input);
 
 const solution = (input) => {
   for (let i = 0; i < input.length; i++) {
-    if (input[i] < 0) {
-      for (let j = i; j >= 0; j--) {
-        if (input[j - 1] > 0) {
-          [input[j], input[j - 1]] = [input[j - 1], input[j]];
-        }
+    for (let j = i - 1; j >= 0; j--) {
+      const tmp = input[j];
+      if (input[j] > tmp) {
+        input[j + 1] = input[j];
+      } else {
+        break;
       }
+
+      if (j == 0) input[j] = tmp;
     }
   }
   console.log(input);
