8e70ca3932eafb23daf2dd0b6743da2e75b64bf2
yechan <noy3928@naver.com>
2022-04-18 22:52:40 +0900

    #98 : 장난꾸러기 현수 - 복습

--- Practice/인프런/정렬,그리디,결정알고리즘/장난꾸러기 현수.js
+++ Practice/인프런/정렬,그리디,결정알고리즘/장난꾸러기 현수.js
@@ -10,3 +10,39 @@ function solution(arr) {
 
 let arr = [120, 125, 152, 130, 135, 135, 143, 127, 160];
 console.log(solution(arr));
+
+/*
+ 1. 현재 현수가 있는 위치를 구한다. i > i +1 인 상황이면 i번째에 현수가 있는 것이다.
+ 2. 그리고 현수의 값을 구한다. 
+ 3. 주어진 학생들을 키순으로 정렬한다.
+ 4. 현수의 원래 번호를 구한다. indexof를 통해서 구한다. 
+ */
+
+const solution = (input) => {
+  let answer = [];
+  let 현수의키 = 0;
+  let 현수의번호 = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] > input[i + 1]) {
+      answer.push(i + 1);
+      현수의키 = input[i];
+      break;
+    }
+  }
+
+  for (let i = 0; i < input.length; i++) {
+    let idx = i;
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[j] < input[idx]) idx = j;
+    }
+    [input[idx], input[i]] = [input[i], input[idx]];
+  }
+
+  현수의번호 = [...input].indexOf(현수의키);
+  answer.push(현수의번호 + 1);
+
+  console.log(answer.join(" "));
+};
+
+solution(input);
--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 let fs = require("fs");
-let input = fs
-  .readFileSync("example.txt")
-  .toString()
-  .split("\n")
-  .map((v) => v.split(" "));
-const [N, M] = input;
+let input = fs.readFileSync("example.txt").toString().split(" ");
 
-function solution(n, m) {
-  const size = n[0];
-  let cache = Array.from({ length: size }, () => 0);
+/*
+ 1. 현재 현수가 있는 위치를 구한다. i > i +1 인 상황이면 i번째에 현수가 있는 것이다.
+ 2. 그리고 현수의 값을 구한다. 
+ 3. 주어진 학생들을 키순으로 정렬한다.
+ 4. 현수의 원래 번호를 구한다. indexof를 통해서 구한다. 
+ */
 
-  m.forEach((item) => {
-    let pos = -1;
-    for (let i = 0; i < size - 1; i++) if (item === cache[i]) pos = i;
-    if (pos === -1) {
-      for (let i = size - 1; i >= 1; i--) {
-        cache[i] = cache[i - 1];
-      }
-    } else {
-      for (let i = pos; i >= 1; i--) {
-        cache[i] = cache[i - 1];
-      }
+// 틀린 답
+
+const solution = (input) => {
+  let answer = [];
+  let 현수의키 = 0;
+  let 현수의번호 = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] > input[i + 1]) {
+      answer.push(i + 1);
+      현수의키 = input[i];
+      break;
     }
-    cache[0] = item;
-  });
-  console.log(cache);
-}
+  }
 
-solution(N, M);
+  for (let i = 0; i < input.length; i++) {
+    let idx = i;
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[j] < input[idx]) idx = j;
+    }
+    [input[idx], input[i]] = [input[i], input[idx]];
+  }
+
+  현수의번호 = [...input].indexOf(현수의키);
+  answer.push(현수의번호 + 1);
+
+  console.log(answer.join(" "));
+};
+
+solution(input);
