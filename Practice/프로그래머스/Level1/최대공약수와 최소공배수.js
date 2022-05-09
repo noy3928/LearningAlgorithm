c389a832807522eb6cfb04d30b13a3d2b9a6a9ec
yechan <noy3928@naver.com>
2022-05-09 18:13:54 +0900

    #125 : 최대공약수와 최소공배수

---
+++ Practice/프로그래머스/Level1/최대공약수와 최소공배수.js
@@ -0,0 +1,5 @@
+function solution(num1, num2) {
+  const gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b))
+  const lcm = (a, b) => (a * b) / gcd(a, b)
+  return [gcd(num1, num2), lcm(num1, num2)]
+}
