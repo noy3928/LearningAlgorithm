b1091dd38202219ed8e9fce164bbd4e7e5f0c1d5
yechan <noy3928@naver.com>
2022-06-30 17:26:03 +0900

    #149 : 부분집합구하기 - 복습

--- Practice/인프런/재귀함수와 스택프레임/부분집합구하기.js
+++ Practice/인프런/재귀함수와 스택프레임/부분집합구하기.js
@@ -1,22 +1,43 @@
 function solution(n) {
-  let answer = [];
-  let ch = Array.from({ length: n + 1 }, () => 0);
+  let answer = []
+  let ch = Array.from({ length: n + 1 }, () => 0)
   function DFS(L) {
     if (L === n + 1) {
-      let tmp = "";
+      let tmp = ""
       for (let i = 1; i <= n; i++) {
-        if (ch[i] === 1) tmp += i + " ";
+        if (ch[i] === 1) tmp += i + " "
       }
-      if (tmp.length > 0) answer.push(tmp.trim());
+      if (tmp.length > 0) answer.push(tmp.trim())
     } else {
-      ch[L] = 1;
-      DFS(L + 1);
-      ch[L] = 0;
-      DFS(L + 1);
+      ch[L] = 1
+      DFS(L + 1)
+      ch[L] = 0
+      DFS(L + 1)
     }
   }
-  DFS(1);
-  return answer;
+  DFS(1)
+  return answer
 }
 
-console.log(solution(3));
+console.log(solution(3))
+
+function solution(n) {
+  let ch = Array.from({ length: n }, () => false)
+  function DFS(L) {
+    if (L == n + 1) {
+      let tmp = []
+      for (let i = 1; i < n + 1; i++) {
+        if (ch[i]) {
+          tmp.push(i)
+        }
+      }
+      console.log(tmp.join(" "))
+    } else {
+      ch[L] = true
+      DFS(L + 1)
+      ch[L] = false
+      DFS(L + 1)
+    }
+  }
+  DFS(1)
+}
