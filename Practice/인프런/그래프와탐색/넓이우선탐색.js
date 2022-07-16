7e279475360a4726fc430a3d88c9e27b79a988fa
yechan <noy3928@naver.com>
2022-07-16 23:28:13 +0900

    #163 : 이진트리 넓이우선탐색

---
+++ Practice/인프런/그래프와탐색/넓이우선탐색.js
@@ -0,0 +1,15 @@
+function solution() {
+  let answer = ""
+  let queue = []
+  queue.push(1)
+  while (queue.length) {
+    let v = queue.shift()
+    answer += v + " "
+    for (let nv of [v * 2, v * 2 + 1]) {
+      if (nv > 7) continue
+      queue.push(nv)
+    }
+  }
+
+  return answer
+}
