e5543595b9a4a19f040e0612b71b324df1c2234e
yechan <noy3928@naver.com>
2022-07-20 07:13:13 +0900

    #169 : 큰 수의 법칙 - other solution

--- Practice/이코테/그리디/큰수의 법칙.js
+++ Practice/이코테/그리디/큰수의 법칙.js
@@ -26,3 +26,26 @@ function solution(m, k, arr) {
 }
 
 solution(M, K, arr)
+
+/*
+시간복잡도 : O(N)
+공간복잡도 : O(1)
+ */
+
+//-------------------- other solution --------------------
+
+function solution(m, k, arr) {
+  const sorted = arr.sort((a, b) => b - a)
+  const maxNum = sorted[0]
+  const nextNum = sorted[1]
+
+  const chunk = maxNum * k + nextNum
+  const quotient = parseInt(m / (k + 1))
+  const remainder = m % (k + 1)
+
+  const answer = quotient * chunk + remainder * maxNum
+
+  console.log(answer)
+}
+
+solution(M, K, arr)
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let input = require("fs").readFileSync("example.txt").toString().split("\n")
 
-const [N, M, K] = input.shift().split(" ")
+const [N, M, K] = input
+  .shift()
+  .split(" ")
+  .map(v => Number(v))
 const arr = input
   .shift()
   .split(" ")
@@ -10,18 +13,13 @@ function solution(m, k, arr) {
   const sorted = arr.sort((a, b) => b - a)
   const maxNum = sorted[0]
   const nextNum = sorted[1]
-  let count = 0
-  let answer = 0
 
-  for (let i = 0; i < m; i++) {
-    if (count < k) {
-      answer += maxNum
-      count++
-    } else {
-      answer += nextNum
-      count = 0
-    }
-  }
+  const chunk = maxNum * k + nextNum
+  const quotient = parseInt(m / (k + 1))
+  const remainder = m % (k + 1)
+
+  const answer = quotient * chunk + remainder * maxNum
+
   console.log(answer)
 }
 
