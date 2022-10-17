151bdfb734ae7cc7d71a7bdb6f2aaee237fb3819
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-17 12:20:11 +0900

    #221 : 올바른 괄호

--- Practice/프로그래머스/level2/올바른괄호.js
+++ Practice/프로그래머스/level2/올바른괄호.js
@@ -10,3 +10,17 @@ function solution(s) {
   if (stack.length !== 0) return (answer = false)
   return answer
 }
+
+function solution(s) {
+  let answer = true
+  let stackCount = 0
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == "(") stackCount++
+    else if (s[i] == ")") {
+      if (stackCount == 0) return (answer = false)
+      else stackCount--
+    }
+  }
+  if (stackCount !== 0) return (answer = false)
+  return answer
+}
