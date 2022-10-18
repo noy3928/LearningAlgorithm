c7df7b68e73433a36e231a6799c68abb872e9746
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-18 10:08:25 +0900

    #224 : 이진변환 반복하기

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 let fs = require("fs")
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt"
-let input = fs.readFileSync(filePath).toString().trim().split("\n")
-input.shift()
-const arr = input.shift().split(" ").map(Number)
+let [N, M] = fs.readFileSync(filePath).toString().trim().split("\n").map(Number)
 
 const isPrime = n => {
   if (n == 1) return false
   if (n == 2) return true
+
   for (let i = 2; i <= Math.ceil(Math.sqrt(n)); i++) {
     if (n % i === 0) {
       return false
@@ -16,12 +15,17 @@ const isPrime = n => {
   return true
 }
 
-const solution = (array, checkPrime) => {
-  let count = 0
-  array.forEach(v => {
-    if (checkPrime(v)) count++
-  })
-  return count
+const solution = (N, M, checkPrime) => {
+  let sum = 0
+  let firstPrime = 0
+  for (let i = N; i <= M; i++) {
+    if (checkPrime(i)) {
+      if (!firstPrime) firstPrime = i
+      sum += i
+    }
+  }
+  if (!sum) return -1
+  return sum + "\n" + firstPrime
 }
 
-console.log(solution(arr, isPrime))
+console.log(solution(N, M, isPrime))
