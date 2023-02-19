0da793a7180a39684f3ca964f0bb94315ff67730
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-02-20 00:09:06 +0900

    add commnet

--- Practice/codewar/selectiveStrReverse.js
+++ Practice/codewar/selectiveStrReverse.js
@@ -12,3 +12,27 @@ function selReverse(array, length) {
     return acc.concat(cur.reverse())
   }, [])
 }
+
+/*
+1. 먼저 주어진 배열을 length 만큼 잘라서 배열로 만든다. 
+2. 그 배열을 순회하면서 각 배열을 reverse한다.
+3. 그리고 그 배열을 하나의 배열로 만든다. 
+*/
+
+// ---------------
+
+function selReverse(array, length) {
+  if (length === 0) {
+    return array
+  }
+  var result = []
+  for (var i = 0; i < array.length; i += length) {
+    var chunk = array.slice(i, i + length)
+    if (chunk.length < length) {
+      result = result.concat(chunk.reverse())
+    } else {
+      result = result.concat(chunk.reverse())
+    }
+  }
+  return result
+}
