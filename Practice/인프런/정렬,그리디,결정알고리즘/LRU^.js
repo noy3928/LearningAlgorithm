62eeb4df1b7e1d97a52d0b4d5189ed1d76b92f1e
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-05-25 22:20:06 +0900

    #복습 : LRU

--- Practice/인프런/정렬,그리디,결정알고리즘/LRU^.js
+++ Practice/인프런/정렬,그리디,결정알고리즘/LRU^.js
@@ -1,41 +1,62 @@
 function solution(size, arr) {
-  let answer = Array.from({ length: size }, () => 0);
-  arr.forEach((x) => {
-    let pos = -1;
-    for (let i = 0; i < size; i++) if (x === answer[i]) pos = i;
+  let answer = Array.from({ length: size }, () => 0)
+  arr.forEach(x => {
+    let pos = -1
+    for (let i = 0; i < size; i++) if (x === answer[i]) pos = i
     if (pos === -1) {
       for (let i = size - 1; i >= 1; i--) {
-        answer[i] = answer[i - 1];
+        answer[i] = answer[i - 1]
       }
     } else {
       for (let i = pos; i >= 1; i--) {
-        answer[i] = answer[i - 1];
+        answer[i] = answer[i - 1]
       }
     }
-    answer[0] = x;
-  });
+    answer[0] = x
+  })
 
-  return answer;
+  return answer
 }
 
-let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
-console.log(solution(5, arr));
+let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7]
+console.log(solution(5, arr))
 
 //----- unshift 방식
 
 function solution(size, arr) {
-  let answer = [];
-  arr.forEach((x) => {
-    let pos = -1;
-    for (let i = 0; i < size; i++) if (x === answer[i]) pos = i;
+  let answer = []
+  arr.forEach(x => {
+    let pos = -1
+    for (let i = 0; i < size; i++) if (x === answer[i]) pos = i
     if (pos === -1) {
-      answer.unshift(x);
-      if (answer.length > size) answer.pop();
+      answer.unshift(x)
+      if (answer.length > size) answer.pop()
     } else {
-      answer.splice(pos, 1);
-      answer.unshift(x);
+      answer.splice(pos, 1)
+      answer.unshift(x)
     }
-  });
+  })
 
-  return answer;
+  return answer
 }
+
+// --------------------------
+
+function solution(n, arr) {
+  const cache = Array.from({ length: n }, () => 0)
+  const cacheHit = new Set()
+
+  for (let i = 0; i < arr.length; i++) {
+    let j
+    if (cacheHit.has(arr[i])) j = cache.indexOf(arr[i])
+    else j = cache.length - 1
+    while (j >= 0) {
+      cache[j] = cache[j - 1]
+      j--
+    }
+    cache[j + 1] = arr[i]
+  }
+  return cache
+}
+
+console.log(solution(5, [1, 2, 3, 2, 6, 2, 3, 5, 7]))
--- Practice/인프런/정렬,그리디,결정알고리즘/회의실 배정.js
+++ Practice/인프런/정렬,그리디,결정알고리즘/회의실 배정.js
@@ -7,41 +7,43 @@
 
 // 나의 풀이
 
-const solution = (input) => {
-  let answer = 1;
+const solution = input => {
+  let answer = 1
   const sortedInput = input.sort((a, b) => {
-    if (a[1] === b[1]) return a[0] - b[0];
-    else return a[1] - b[1];
-  });
+    if (a[1] === b[1]) return a[0] - b[0]
+    else return a[1] - b[1]
+  })
 
-  let end = sortedInput[0][1];
+  let end = sortedInput[0][1]
   for (let i = 1; i < sortedInput.length; i++) {
     if (Number(input[i][0]) >= Number(end)) {
-      answer++;
-      end = input[i][1];
+      answer++
+      end = input[i][1]
     }
   }
-  console.log(answer);
-};
+  console.log(answer)
+}
 
-solution(input);
+solution(input)
 
 //---- 선생님 풀이
 
 function solution(meeting) {
-  let answer = 0;
+  let answer = 0
   meeting.sort((a, b) => {
-    if (a[1] === b[1]) return a[0] - b[0];
-    else return a[1] - b[1];
-  });
-  let et = 0;
+    // 우선 시간대별로 정렬한다.
+    if (a[1] === b[1]) return a[0] - b[0]
+    else return a[1] - b[1]
+  })
+  let et = 0 // 마지막 시간을 저장할 변수를 만든다.
   for (let x of meeting) {
     if (x[0] >= et) {
-      answer++;
-      et = x[1];
+      // x[0]은 시작시간, et은 마지막 시간. 시작시간이 마지막 시간보다 크거나 같다면,
+      answer++ // answer에 + 1을 해주고
+      et = x[1] // 마지막 시간을 x[1]로 바꿔준다.
     }
   }
-  return answer;
+  return answer
 }
 
 let arr = [
@@ -50,5 +52,5 @@ let arr = [
   [3, 5],
   [4, 6],
   [5, 7],
-];
-console.log(solution(arr));
+]
+console.log(solution(arr))
